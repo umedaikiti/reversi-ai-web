826d34b002f81a3c21ddcd0832f94e79e39d418b
Satoshi Kura <umedaikiti@gmail.com>
2018-04-18 20:16:59 +0900

    Improve index.js

--- src/index.js
+++ src/index.js
@@ -4,22 +4,46 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+function info_string(data) {
+	var info = '';
+	if(data.turn === 1) info += 'Your turn.';
+	else if(data.turn === -1) info += "AI's turn.";
+	else {
+		var p = 0;
+		var o = 0;
+		for(var i=0;i<8;i++) for(var j=0;j<8;j++) {
+			if(data.board[i][j] === 1) p++;
+			if(data.board[i][j] === -1) o++;
+		}
+		info += 'Game over. ';
+		if(p > o) {
+			info += 'You win!';
+		} else if (p < o) {
+			info += 'You lose!';
+		} else {
+			info += 'Draw.';
+		}
+		info += ` (${p} vs ${o})`;
+	}
+	return info;
+}
+
 if(window.Worker) {
 	var worker = new Worker("worker.js");
-//	worker.postMessage({type: 'reset'});
+	//	worker.postMessage({type: 'reset'});
 	var clickValid = false;
 	const handleClick = (i, j) => {
-	  return e => {
-		  e.preventDefault();
-		  console.log(`clicked: (${i}, ${j})`);
-		  if(clickValid) {
-		  	worker.postMessage({type: 'move', x: i, y: j});
-		  	clickValid = false;
-		  } else {
-			  console.log('canceled');
-		  }
-	  };
-  	};
+		return e => {
+			e.preventDefault();
+			console.log(`clicked: (${i}, ${j})`);
+			if(clickValid) {
+				worker.postMessage({type: 'move', x: i, y: j});
+				clickValid = false;
+			} else {
+				console.log('canceled');
+			}
+		};
+	};
 	const reset = e => {
 		e.preventDefault();
 		if(window.confirm('Are you sure you want to start a new game?')) {
@@ -29,26 +53,7 @@ if(window.Worker) {
 
 	worker.onmessage = e => {
 		console.log('update');
-		var info = '';
-		if(e.data.turn === 1) info += 'Your turn.';
-		else if(e.data.turn === -1) info += "AI's turn.";
-		else {
-			var p = 0;
-			var o = 0;
-			for(var i=0;i<8;i++) for(var j=0;j<8;j++) {
-				if(e.data.board[i][j] === 1) p++;
-				if(e.data.board[i][j] === -1) o++;
-			}
-			info += 'Game over. ';
-			if(p > o) {
-				info += 'You win!';
-			} else if (p < o) {
-				info += 'You lose!';
-			} else {
-				info += 'Draw.';
-			}
-			info += ` (${p} vs ${o})`;
-		}
+		var info = info_string(e.data);
 		ReactDOM.render(<App handleClick={handleClick} reset={reset} data={e.data} info={info} />, document.getElementById('root'));
 		if(e.data.turn === 1) clickValid = true;
 	}
