666808e341a82d33a3db94b873312f2171db4c23
Satoshi Kura <umedaikiti@gmail.com>
2018-04-18 20:17:15 +0900

    Add a test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,33 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+	const handleClick = (i, j) => { return e => {}; };
+	const reset = e => {};
+  let board = [
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,1,-1,0,0,0],
+    [0,0,0,-1,1,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0]
+  ];
+  let valid_move = [
+    [true, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false]
+  ];
+  let state = {last: null, turn: 1, board: board, valid_move: valid_move};
+  const info = 'test';
+  ReactDOM.render(<App handleClick={handleClick} reset={reset} data={state} info={info} />, div);
+  state.last = {x: 1, y: 1};
+  state.turn = -1;
+  ReactDOM.render(<App handleClick={handleClick} reset={reset} data={state} info={info} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
