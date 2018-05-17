6d3846201c66ce1b77eeab07246872d0cb2d8e7a
Satoshi Kura <umedaikiti@gmail.com>
2018-05-17 11:54:44 +0900

    Fix indention

--- src/App.js
+++ src/App.js
@@ -19,36 +19,36 @@ class App extends Component {
 	renderSquare = (i, j) => {
 		var cls = "row";
 		if(this.props.data.last !== null && i === this.props.data.last.x && j === this.props.data.last.y) cls += " last-move";
-	  return (
-		<div className={cls} key={'row' + i + '-' + j} onClick={this.props.handleClick(i, j)}>
-		  {this.renderDisk(this.props.data.board[i][j], i, j)}
-		</div>
-	  );
-  }
-  renderCol(i) {
-	  var list = [];
-	  var j;
+		return (
+			<div className={cls} key={'row' + i + '-' + j} onClick={this.props.handleClick(i, j)}>
+			{this.renderDisk(this.props.data.board[i][j], i, j)}
+			</div>
+		);
+	}
+	renderCol(i) {
+		var list = [];
+		var j;
 		for(j=0;j<8;j++) list.push(this.renderSquare(i, j));
-	  return (
-		<div  key={'col' + i} className="col">
-		  {list}
-		</div>
-	  );
-  }
-  render() {
-	  var list = [];
-	  var i;
-	  for(i=0;i<8;i++) list.push(this.renderCol(i));
-    return (
-      <div className="App">
-		<div className="board">
-		{list}
-		</div>
-		<input type="button" value="reset" onClick={this.props.reset} />
-		<div id="info">{this.props.info}</div>
-      </div>
-    );
-  }
+		return (
+			<div  key={'col' + i} className="col">
+			{list}
+			</div>
+		);
+	}
+	render() {
+		var list = [];
+		var i;
+		for(i=0;i<8;i++) list.push(this.renderCol(i));
+		return (
+			<div className="App">
+			<div className="board">
+			{list}
+			</div>
+			<input className="reset-button" type="button" value="reset" onClick={this.props.reset} />
+			<div className="info" id="info">{this.props.info}</div>
+			</div>
+		);
+	}
 }
 
 export default App;
